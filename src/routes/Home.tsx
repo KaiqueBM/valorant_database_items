f41c19a878c20eb3daa0116d0e9d31d6006dd7a7
Kaique Miranda <exhardkaique@gmail.com>
2023-03-28 12:14:20 -0300

    Feat: Último agente

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -7,6 +7,7 @@ import "../styles/home.css";
 const Home = () => {
   const [bundleAtual, setBundleAtual] = useState([]);
   const [weaponAtual, setWeaponAtual] = useState([]);
+  const [agenteAtual, setAgenteAtual] = useState([]);
 
   const getBundleAtual = async () => {
     try {
@@ -14,13 +15,15 @@ const Home = () => {
         "/bundles/b7d754d4-44aa-4663-afc3-84a5cccc3c9d?language=pt-BR"
       );
       const response1 = await api.get("/weapons/skins?language=pt-BR");
+      const response2 = await api.get("/agents/e370fa57-4757-3604-3648-499e1f642d3f?language=pt-BR");
       const data = response.data;
       const data1 = response1.data;
+      const data2 = response2.data
       const data_filter = data1.data.filter(filter_weapon);
-      console.log(data_filter);
-      console.log(data.data);
+      console.log(data2.data);
       setBundleAtual(data.data);
       setWeaponAtual(data_filter);
+      setAgenteAtual(data2.data)
     } catch (error) {
       console.log(error);
     }
@@ -53,13 +56,13 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="bg-vava">
-        <div className="text-center pt-5 font-raj font-bold text-7xl">
+      <div className="bg-vava py-16">
+        <div className="text-center font-raj font-bold text-8xl">
           BUNDLE ATUAL DA LOJA
         </div>
         <div className="flex flex-row justify-center"></div>
 
-        <div className="flex flex-col p-4">
+        <div className="flex flex-col py-12">
           <div className="flex flex-row items-end justify-center border-4 h-full">
             {weaponAtual.length === 0 ? (
               <p>Carregando...</p>
@@ -74,14 +77,51 @@ const Home = () => {
               ))
             )}
           </div>
-          <div className="mx-auto mt-2"><button className="bg-verm px-10 py-4 font-raj font-semibold text-2xl rounded-lg">VER OUTROS BUNDLES</button></div>
+          <div className="mx-auto mt-2 pt-12"><button className="bg-verm px-10 py-4 font-raj font-semibold text-2xl rounded-lg">VER OUTROS BUNDLES</button></div>
         </div>
       </div>
 
-    <div>
-        <div className="text-center pt-5 font-raj font-bold text-7xl text-vava">
+    <div className=" bg-purple-500 pt-16">
+        <div className="text-center font-raj font-bold text-8xl text-vava">
           ÚLTIMO AGENTE LANÇADO
         </div>
+
+
+        <div className="flex flex-row flex-wrap justify-center items-center m-10">
+                <div className="w-1/3">
+                {agenteAtual.length === 0 ? (<p>Carregando...</p>) : (
+                        <div className="" key={agenteAtual.uuid}>
+							<img src={agenteAtual.background} width="100%" className="mx-auto opacity-90 -mt-28"  />
+							<div className="relative"> 
+                            <img className="absolute bottom-20" src={agenteAtual.bustPortrait} />
+							</div>
+                            
+                        </div>
+                )}
+                </div>
+                <div className="w-1/2 text-vava font-fira">
+                    {agenteAtual.length === 0 ? (<p>Carregando...</p>) : (
+                        <>
+                    <div className="text-6xl uppercase font-semibold pt-10 flex justify-between">{agenteAtual.displayName} <img className="h-14" src={agenteAtual.role.displayIcon} /></div>
+                    <div className="text-2xl text-justify pt-10">{agenteAtual.description}</div>
+                    
+                    <div className="flex w-full justify-between">
+                    {agenteAtual.abilities.map((abilidades)=>(
+                        <div className="pt-10">
+                        <img src={abilidades.displayIcon} />
+                        <div className="text-center">{abilidades.displayName}</div>
+                        </div>
+                    ))}
+                    
+                    </div>
+                    </>
+                    )}
+                </div>
+                
+            </div>
+            
+
+
     </div>
 
 
