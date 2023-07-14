572973daef760e38e363a82f041bc799917a60de
Kaique Miranda <exhardkaique@gmail.com>
2023-07-14 16:01:58 -0300

    feat: Adicionado variantes das coleções

--- src/routes/BundlesInfo.tsx
+++ src/routes/BundlesInfo.tsx
@@ -22,52 +22,80 @@ const BundlesInfo = () => {
 		}
 	};
 
-		useEffect(() => {
-			getBundleAtual();
-		}, []);
+	useEffect(() => {
+		getBundleAtual();
+	}, []);
 
-		function filter_weapon(props: any) {
-			if (props.displayName.includes(displayName)) {
-				return props;
-			}
+	function filter_weapon(props: any) {
+		if (props.displayName.includes(displayName)) {
+			return props;
 		}
+	}
 
-		return (
-			<>
+	function formatarTextoVariante(nome: string){
+		const nomeSplit = nome.split("\n")
+		const nomeFormatado = nomeSplit[1]
+		return nomeFormatado?.replace("(","")?.replace(")","")
+	}
+
+
+	return (
+		<>
 			{bundleAtual.length === 0 ? (
-					<p>Carregando...</p>
-				  ) : (
+				<p>Carregando...</p>
+			) : (
 
-					<div className="bg-vava py-16">
-      <div className="text-center font-raj font-bold md:text-8xl text-6xl">
-        Teste
-      </div>
+				<div className="bg-vava py-16">
+					<div className="text-center font-raj font-bold md:text-8xl text-6xl">
+						Bundle {displayName}
+					</div>
 
-	  {bundleAtual.map((weapon: any) => (
-		<div className="p-5 " key={weapon.uuid}>
-		<img className="h-20 mx-auto" src={weapon.displayIcon} />
-		<p className="text-center text-black font-fira text-xl">
-		  {weapon.displayName}
-		</p>
-	  </div>
+					<div className="flex flex-col py-12 justify-center">
+						{bundleAtual.map((weapon: any) => (
+							<div className="p-5 " key={weapon.uuid}>
+								<img className="h-20 mx-auto" src={weapon.displayIcon} />
+								<p className="text-center text-black font-fira text-xl">
+									{weapon.displayName}
+								</p>
+								<p>Níveis:</p>
+								<div className="flex flex-row">
+								{weapon.chromas.map((chromas: any, index: number) => (
+									<div>
+										{index !== 0 && (
+											<>
+												<p>{formatarTextoVariante(chromas.displayName)}</p>
+												<img className="h-20 mx-auto" src={chromas.displayIcon} />
+												<video autoPlay muted controls src={chromas.streamedVideo} />
+											</>
+										)}
+									</div>
+								))}
+								</div>
+							</div>
 
-	  ))}
 
-      
-        <div className="mx-auto mt-2 pt-12">
-        <Link to={`/bundles`}>
-          <button className="bg-verm px-10 py-4 font-raj font-semibold text-2xl rounded-lg">
-            VER OUTROS BUNDLES
-          </button>
-          </Link>
-        </div>
-      </div>
 
-				  )}
-				  </>
 
-			
-		)
+
+						))}
+
+
+						<div className="mx-auto mt-2 pt-12">
+							<Link to={`/bundles`}>
+								<button className="bg-verm px-10 py-4 font-raj font-semibold text-2xl rounded-lg">
+									VER OUTROS BUNDLES
+								</button>
+							</Link>
+						</div>
+
+					</div>
+				</div>
+
+			)}
+		</>
+
+
+	)
 }
 
 
