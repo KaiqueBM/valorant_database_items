eb5ae77902d7d5af5cda0a54437fe6c6d7f987d7
Kaique Miranda <exhardkaique@gmail.com>
2023-07-19 17:18:21 -0300

    feat: Melhorias na single dos bundles e configurado a parte responsiva

--- src/routes/BundlesInfo.tsx
+++ src/routes/BundlesInfo.tsx
@@ -34,10 +34,10 @@ const BundlesInfo = () => {
 		}
 	}
 
-	function formatarTextoVariante(nome: string){
+	function formatarTextoVariante(nome: string) {
 		const nomeSplit = nome.split("\n")
 		const nomeFormatado = nomeSplit[1]
-		return nomeFormatado?.replace("(","")?.replace(")","")
+		return nomeFormatado?.replace("(", "")?.replace(")", "")
 	}
 
 
@@ -54,39 +54,52 @@ const BundlesInfo = () => {
 
 					<div className="flex flex-col py-12 justify-center">
 						{bundleAtual.map((weapon: any) => (
-							<div className="p-5 bg-vava container rounded-xl" key={weapon.uuid}>
-								<img className="h-40 mx-auto mt-6" src={weapon.displayIcon} />
+							<div className="p-5 bg-vava container xl:rounded-xl" key={weapon.uuid}>
+								<img className="xl:h-40 mx-auto mt-6" src={weapon.displayIcon} />
 								<p className="text-center text-black font-fira text-5xl font-semibold mt-2">
 									{weapon.displayName}
 								</p>
-								<p className="text-verm font-fira text-4xl font-semibold mt-2 mx-2">Níveis:</p>
-								<div className="flex flex-row">
-								{weapon.levels.map((nivel: any, index: number) => (
-									<div>
-										{index !== 0 && (
-											<div className="mx-2">
-												<p className="text-center text-black font-raj text-xl font-semibold">{nivel.displayName}</p>
-												<video className="rounded-lg" autoPlay muted controls src={nivel.streamedVideo} />
-											</div>
-										)}
-									</div>
-								))}
+								{weapon.levels.length > 1 ? (<p className="text-zinc-600 text-center font-fira md:text-2xl text-xl mt-2">Contém {weapon.levels.length} níveis</p>) : (<p className="text-zinc-600 text-center font-fira md:text-2xl text-xl mt-2">Não contém níveis</p>)}
+								{weapon.chromas.length > 1 ? (<p className="text-zinc-600 text-center font-fira md:text-2xl text-xl">{weapon.chromas.length} variantes</p>) : (<p className="text-zinc-600 text-center font-fira md:text-2xl text-xl">Sem variantes</p>)}
+								<hr className="border-4 border-verm rounded-full w-5/6 mx-auto my-5"></hr>
+								
+								{weapon.levels.length > 1 && (
+								<>
+								<p className="text-verm font-fira text-5xl font-semibold xl:mb-2 mb-10 mx-2 xl:text-start text-center uppercase">Níveis:</p>
+								<div className="flex xl:flex-row flex-col">
+									{weapon.levels.map((nivel: any, index: number) => (
+										<div>
+											{index !== 0 && (
+												<div className="xl:mx-2 mx-10 xl:mb-0 mb-8">
+													<p className="text-center text-black font-raj text-xl font-semibold">{nivel.displayName}</p>
+													<video className="rounded-lg" autoPlay muted controls src={nivel.streamedVideo} />
+												</div>
+											)}
+										</div>
+									))}
 								</div>
-								<p className="text-verm font-fira text-4xl font-semibold mt-10 mx-2">Variantes:</p>
-								<div className="flex flex-row">
-								{weapon.chromas.map((chromas: any, index: number) => (
-									<div>
-										{index !== 0 && (
-											<div className="mx-2">
-												<img className="h-24 mx-auto mt-6" src={chromas.displayIcon} />
-												<div className="text-center"><img className="rounded-lg inline-block h-6 mx-1" src={chromas.swatch} /><span className="text-center text-black font-raj text-xl font-semibold">{formatarTextoVariante(chromas.displayName)}</span></div>
-												<video className="rounded-lg mt-2" autoPlay muted controls src={chromas.streamedVideo} />
-											</div>
-										)}
-									</div>
-									
-								))}
+								<hr className="border-4 border-verm rounded-full w-5/6 mx-auto my-5"></hr>
+								</>
+								)}
+								{weapon.chromas.length > 1 && (
+								<>
+								<p className="text-verm font-fira text-5xl font-semibold xl:mb-2 mb-10 mx-2 xl:text-start text-center uppercase">Variantes:</p>
+								<div className="flex xl:flex-row flex-col">
+									{weapon.chromas.map((chromas: any, index: number) => (
+										<div>
+											{index !== 0 && (
+												<div className="xl:mx-2 mx-10 xl:mb-0 mb-8">
+													<img className=" mx-auto mt-6" src={chromas.displayIcon} />
+													<div className="text-center"><img className="rounded-lg inline-block h-6 mx-1" src={chromas.swatch} /><span className="text-center text-black font-raj text-xl font-semibold">{formatarTextoVariante(chromas.displayName)}</span></div>
+													<video className="rounded-lg mt-2" autoPlay muted controls src={chromas.streamedVideo} />
+												</div>
+											)}
+										</div>
+
+									))}
 								</div>
+								</>
+								)}
 							</div>
 
 
